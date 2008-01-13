21b60e46788d44db70f3229b8cdcdd8b85b1f706
lewisvance@gmail.com <lewisvance@gmail.com@0af07104-5242-0410-b30e-b11728ae724a>
2008-01-13 02:44:32 +0000

    rewwrite

---
+++ trunk/ConsoleTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("a")]
+[assembly: AssemblyProduct("ConsoleTests")]
+[assembly: AssemblyCopyright("Copyright © a 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("36f6c214-050c-432d-a935-bfb43d8e688a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ trunk/libeveapi/Network.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml;
+
+namespace libeveapi
+{
+    public class Network
+    {
+        public static XmlDocument GetXml(string url)
+        {
+            WebClient wc = new WebClient();
+            Stream s = wc.OpenRead(url);
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(s);
+            return xmlDoc;
+        }
+    }
+
+
+}
