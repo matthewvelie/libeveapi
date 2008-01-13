f233185406c64385a754e441192ee68b0fc21f5f
lewisvance@gmail.com <lewisvance@gmail.com@0af07104-5242-0410-b30e-b11728ae724a>
2008-01-13 22:31:51 +0000

    added CharacterListExamples

--- trunk/ConsoleTests/Program.cs
+++ trunk/ConsoleTests/Program.cs
@@ -5,26 +5,13 @@ using System.Xml;
 
 using libeveapi;
 
-namespace Tests
+namespace ConsoleTests
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(String[] args)
         {
-            XmlDocument xmlDoc = Network.GetXml("http://localhost/eveapi/corp/AssetList.xml");
-            foreach (XmlNode node in xmlDoc.SelectNodes("//rowset[@name='assets']/row"))
-            {
-                PrintItem(node, "");
-            }
-        }
-
-        static void PrintItem(XmlNode item, string indent)
-        {
-            Console.WriteLine("{0}{1}", indent, item.Attributes["itemID"].InnerText);
-            foreach (XmlNode containedItem in item.SelectNodes("./rowset[@name='contents']/row"))
-            {
-                PrintItem(containedItem, indent + "  ");
-            }
+            CharacterListExamples.PrintCharacterList();
         }
     }
 }
