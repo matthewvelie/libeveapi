fd68f636245ed4f96e1bbf35543a77737732d2e8
Lewis Vance <lewisvance@gmail.com>
2008-01-13 08:51:29 +0000

    added Constants.cs and moved xml files around

---
+++ libeveapi/Constants.cs
@@ -0,0 +1,27 @@
+#define USE_LOCALHOST
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace libeveapi
+{
+    class Constants
+    {
+#if USE_LOCALHOST
+        internal static readonly string ApiPrefix = "http://localhost/eveapi";
+        internal static readonly string CharacterAccountBalance = "/char/AccountBalance.xml";
+        internal static readonly string CorpAccountBalance = "/corp/AccountBalance.xml";
+        internal static readonly string CharacterList = "/account/Characters.xml";
+        internal static readonly string StarbaseDetails = "/corp/StarbaseDetail.xml";
+        internal static readonly string StarbaseList = "/corp/StarbaseList.xml";
+#else
+        internal static readonly string ApiPrefix = "http://api.eve-online.com";
+        internal static readonly string CharacterAccountBalance = "/char/AccountBalance.xml.aspx";
+        internal static readonly string CorpAccountBalance = "/corp/AccountBalance.xml.aspx";
+        internal static readonly string CharacterList = "/account/Characters.xml.aspx";
+        internal static readonly string StarbaseDetails = "/corp/StarbaseDetail.xml.aspx";
+        internal static readonly string StarbaseList = "/corp/StarbaseList.xml.aspx";
+#endif
+    }
+}
