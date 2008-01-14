3822e3813c18f48ed3191d5ee4b68134194935df
Matthew Velie <matthewvelie@gmail.com>
2008-01-14 16:59:02 +0000

    Added RefTypes which is used for the journal entries

---
+++ libeveapi/ResponseObjects/RefTypes.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace libeveapi
+{
+    public class RefTypes : ApiResponse
+    {
+        public SerializableDictionary<int, string> RefTypes = new SerializableDictionary<string, string>();
+
+        /// <summary>
+        /// Create an ErrorList by parsing an XmlDocument response from the eveapi
+        /// </summary>
+        /// <param name="xmlDoc"></param>
+        /// <returns></returns>
+        public static RefTypes FromXmlDocument(XmlDocument xmlDoc)
+        {
+            RefTypes refTypes = new RefTypes();
+            refTypes.ParseCommonElements(xmlDoc);
+
+            foreach (XmlNode refTypeNode in xmlDoc.SelectNodes("//rowset[@name='reftypes']/row"))
+            {
+                refTypes.RefTypes.Add(Convert.ToInt32(refTypeNode.Attributes["refTypeID"].InnerText), refTypeNode.Attributes["refTypeName"].InnerText);
+            }
+
+            return refTypes;
+        }
+
+        /// <summary>
+        /// Returns the description for the specified error code
+        /// </summary>
+        /// <param name="errorCode"></param>
+        /// <returns></returns>
+        public string GetReferenceTypeNameForID(int referenceTypeID)
+        {
+            try
+            {
+                return RefTypes[referenceTypeID];
+            }
+            catch (KeyNotFoundException)
+            {
+                return "Unknown Reference Type ID";
+            }
+        }
+    }
+}
