f2788345b87dfa9bedb4a883560d9a6a8eca56e0
matthewvelie <matthewvelie@0af07104-5242-0410-b30e-b11728ae724a>
2008-01-14 17:15:19 +0000

    Updated with new URL's for the new response objects

--- trunk/libeveapi/Constants.cs
+++ trunk/libeveapi/Constants.cs
@@ -15,6 +15,13 @@ namespace libeveapi
         public static string ErrorList = "/eve/ErrorList.xml.aspx";
         public static string CharAssetList = "/char/AssetList.xml.aspx";
         public static string CorpAssetList = "/corp/AssetList.xml.aspx";
+        public static string CharIndustryJobs = "/char/IndustryJobs.xml.aspx";
+        public static string CorpIndustryJobs = "/corp/IndustryJobs.xml.aspx";
+        public static string CharJournalEntries = "/char/WalletJournal.xml.aspx";
+        public static string CorpJournalEntries = "/corp/WalletJournal.xml.aspx";
+        public static string CharMarketTransactions = "/char/WalletTransactions.xml.aspx";
+        public static string CorpMarketTransactions = "/corp/WalletTransactions.xml.aspx";
+        public static string RefTypesList = "/eve/RefTypes.xml.aspx";
 
         // Not part of the eve-api - used for unit tests
         public static string ExampleError = "/Error.xml.aspx";
