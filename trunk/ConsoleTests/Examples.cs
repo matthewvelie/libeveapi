39c13fc6d2b1f6f4427c9dbc84ee6967c9acb618
lewisvance@gmail.com <lewisvance@gmail.com@0af07104-5242-0410-b30e-b11728ae724a>
2008-01-14 02:23:11 +0000

    should only need to change ApiPrefix now

--- trunk/ConsoleTests/Examples.cs
+++ trunk/ConsoleTests/Examples.cs
@@ -24,14 +24,6 @@ namespace ConsoleTests
         public static void UseLocalUrls()
         {
             Constants.ApiPrefix = "http://localhost/eveapi";
-            Constants.CharacterAccountBalance = "/char/AccountBalance.xml";
-            Constants.CorpAccountBalance = "/corp/AccountBalance.xml";
-            Constants.CharacterList = "/account/Characters.xml";
-            Constants.StarbaseDetails = "/corp/StarbaseDetail.xml";
-            Constants.StarbaseList = "/corp/StarbaseList.xml";
-            Constants.ErrorList = "/eve/ErrorList.xml";
-            Constants.CharAssetList = "/char/AssetList.xml";
-            Constants.CorpAssetList = "/corp/AssetList.xml";
         }
 
         public static void PrintStarbaseList()
--- trunk/UnitTests/Utility.cs
+++ trunk/UnitTests/Utility.cs
@@ -11,14 +11,6 @@ namespace UnitTests
         public static void UseLocalUrls()
         {
             Constants.ApiPrefix = "http://localhost/eveapi";
-            Constants.CharacterAccountBalance = "/char/AccountBalance.xml";
-            Constants.CorpAccountBalance = "/corp/AccountBalance.xml";
-            Constants.CharacterList = "/account/Characters.xml";
-            Constants.StarbaseDetails = "/corp/StarbaseDetail.xml";
-            Constants.StarbaseList = "/corp/StarbaseList.xml";
-            Constants.ErrorList = "/eve/ErrorList.xml";
-            Constants.CharAssetList = "/char/AssetList.xml";
-            Constants.CorpAssetList = "/corp/AssetList.xml";
         }
     }
 }
