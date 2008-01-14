7c6f16c2299996173e121a85ef8fb2421f718a4f
Lewis Vance <lewisvance@gmail.com>
2008-01-14 00:33:29 +0000

    examples and consistency

--- ConsoleTests/Examples.cs
+++ ConsoleTests/Examples.cs
@@ -15,6 +15,8 @@ namespace ConsoleTests
             AccountBalanceExamples.PrintAccounts();
             PrintStarbaseList();
             PrintStarbaseDetail();
+            ErrorListExample();
+            PrintAllAssets();
         }
 
         public static void PrintStarbaseList()
@@ -34,6 +36,30 @@ namespace ConsoleTests
             foreach (FuelListItem fli in starbaseDetail.FuelList)
             {
                 Console.WriteLine("TypeID: {0} Quantity: {1}", fli.TypeId, fli.Quantity);
+            }
+        }
+
+        public static void ErrorListExample()
+        {
+            ErrorList errorList = EveApi.GetErrorList();
+            Console.WriteLine("{0}", errorList.GetMessageForErrorCode("100"));
+        }
+
+        public static void PrintAllAssets()
+        {
+            AssetList assetList = EveApi.GetAssetList(AssetListType.Corporation, "userId", "characterId", "fullApiKey");
+            foreach (AssetListItem ali in assetList.AssetListItems)
+            {
+                PrintAsset(ali);
+            }
+        }
+
+        public static void PrintAsset(AssetListItem ali)
+        {
+            Console.WriteLine("itemID: {0} quantity: {1}", ali.ItemId, ali.Quantity);
+            foreach (AssetListItem childAsset in ali.Contents)
+            {
+                PrintAsset(childAsset);
             }
         }
     }
