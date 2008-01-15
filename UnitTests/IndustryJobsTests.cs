0d680b5f2f5ff778b1b0e7b950aace0dc064a301
Matthew Velie <matthewvelie@gmail.com>
2008-01-15 04:25:32 +0000

    Added Unit Test for Industry Jobs

--- UnitTests/IndustryJobsTests.cs
+++ UnitTests/IndustryJobsTests.cs
@@ -19,19 +19,48 @@ namespace UnitTests
         [Test]
         public void GetCharIndustryJobsTest()
         {
+            IndustryJobList jobList = EveApi.GetIndustryJobList(IndustryJobListType.Character, "userID", "characterID", "fullApiKey");
 
+            Assert.AreEqual(22, jobList.IndustryJobListItems.Length);
+
+            Assert.AreEqual(444, jobList.IndustryJobListItems[0].jobID);
+            Assert.AreEqual(60010783, jobList.IndustryJobListItems[0].containerID);
+            Assert.AreEqual(60010783, jobList.IndustryJobListItems[0].installedItemLocationID);
         }
 
         [Test]
         public void GetCorpIndustryJobsTest()
         {
+            IndustryJobList jobList = EveApi.GetIndustryJobList(IndustryJobListType.Corporation, "userID", "characterID", "fullApiKey");
 
+            Assert.AreEqual(22, jobList.IndustryJobListItems.Length);
+
+            Assert.AreEqual(444, jobList.IndustryJobListItems[0].jobID);
+            Assert.AreEqual(60010783, jobList.IndustryJobListItems[0].containerID);
+            Assert.AreEqual(60010783, jobList.IndustryJobListItems[0].installedItemLocationID);
         }
 
         [Test]
         public void IndustryJobsPersist()
         {
+            ResponseCache.Clear();
 
+            IndustryJobList industryJobList = EveApi.GetIndustryJobList(IndustryJobListType.Corporation, "userId", "charId", "apiKey");
+            ResponseCache.Save("ResponseCache.xml");
+            ResponseCache.Clear();
+            ResponseCache.Load("ResponseCache.xml");
+            IndustryJobList cachedIndustryJobList = EveApi.GetIndustryJobList(IndustryJobListType.Corporation, "userId", "charId", "apiKey");
+
+            Assert.AreEqual(industryJobList.CachedUntilLocal, cachedIndustryJobList.CachedUntilLocal);
+
+            
+            for (int i = 0; i < industryJobList.IndustryJobListItems.Length; i++)
+            {
+                Assert.AreEqual(industryJobList.IndustryJobListItems[i].jobID, cachedIndustryJobList.IndustryJobListItems[i].jobID);
+                Assert.AreEqual(industryJobList.IndustryJobListItems[i].assemblyLineID, cachedIndustryJobList.IndustryJobListItems[i].assemblyLineID);
+                Assert.AreEqual(industryJobList.IndustryJobListItems[i].completed, cachedIndustryJobList.IndustryJobListItems[i].completed);
+            }
+            
         }
     }
 }
