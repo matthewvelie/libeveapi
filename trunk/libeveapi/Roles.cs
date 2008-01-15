b3bf2c1de9877c62613ec4ff30135ed6dbf4dd88
matthewvelie <matthewvelie@0af07104-5242-0410-b30e-b11728ae724a>
2008-01-15 04:40:24 +0000

    Added new roles and updated descriptions

--- trunk/libeveapi/Roles.cs
+++ trunk/libeveapi/Roles.cs
@@ -129,49 +129,105 @@ namespace libeveapi
         /// 
         /// </summary>
         [Name("Director")]
-        [Description("Director Description")]
+        [Description("A director is, for most intents and purposes, the same as a CEO in many respects. They can hire and fire members and change job descriptions (assigning both roles and grantable roles).")]
+        Director = 1,
+
+        /* The roles below exist but at current time their bitmask is unknown.
+         * 
+         */
+
+        /*
+        /// <summary>
+        /// 
+        /// </summary>
+        [Name("Config Equipment")]
+        [Description("This role allows the holder to anchor, unanchor, rename, and configure various types of objects in space.")]
         Director = 1,
 
         /// <summary>
         /// 
         /// </summary>
+        [Name("Config Starbase")]
+        [Description("This role allows the holder to perform star base configuration.")]
+        Director = 1,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Name("Junior Accountant")]
+        [Description("The junior accountant is a cur down version of the accountant role. It essentially allows the holder to view the data that an accountant views. However it does not allow the holder to perform the actions that an accountant can.")]
+        Director = 1,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Name("Personnel Manager")]
+        [Description("A personnel manager can process applications and sign up new members. ")]
+        Director = 1,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Name("Rent Factory")]
+        [Description("This role allows the holder to rent factory slots for the corporation. ")]
+        Director = 1,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Name("Rent Office")]
+        [Description("The role Rent Office allows the holder to rent offices for the corporation. ")]
+        Director = 1,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Name("Rent Research Facility")]
+        [Description("This role allows the holder to rent research facilities for the corporation. ")]
+        Director = 1,
+
+        */
+
+        /// <summary>
+        /// 
+        /// </summary>
         [Name("Personel Manager")]
-        [Description("Personnel Manager Description")]
+        [Description("A personnel manager can process applications and sign up new members.")]
         PersonnelManager = 1 << 7,
 
         /// <summary>
         /// 
         /// </summary>
         [Name("Accountant")]
-        [Description("Accountant Description")]
+        [Description("Accountants can monitor and manage the financial affairs of the corporation. They can view corporation bills in the NeoCom “Bills” panel.")]
         Accountant = 1 << 8,
 
         /// <summary>
         /// 
         /// </summary>
         [Name("Security Manager")]
-        [Description("Security Manager Description")]
+        [Description("The role security officer allows the holder to view the contents of the deliveries window in station. This is the window where items bought by the corporation at a station are placed. If the corporation has a corporate hangar at a station, the role Security Officer grants view access to the contents of the members' hangars. This access also facilitates the placing of items into the members' hangars.")]
         SecurityManager = 1 << 9,
 
         /// <summary>
         /// 
         /// </summary>
         [Name("Factory Manager")]
-        [Description("Factory Manager Description")]
+        [Description("The factory manager can eject blueprints from corporation-owned factories and research slots that belong to members. Additionally, they can see what is going on in any corporate factory and research slot. They have the ability to manufacture items for the corporation using blueprints and minerals from the corporation’s hangar floor and research blueprints in research slots. ")]
         FactoryManager = 1 << 10,
 
         /// <summary>
         /// 
         /// </summary>
         [Name("Station Manager")]
-        [Description("Station Manager Description")]
+        [Description("As the name implies, station managers can perform station management tasks for corporation-owned facilities. These tasks are varied and include adjusting the standing-based modifiers for allowing docking and the charges for it, as well as permission to access factories, the reprocessing plant, the repair shop, and also how modifiers should be applied for costs based upon standings for these tasks.")]
         StationManager = 1 << 11,
 
         /// <summary>
         /// 
         /// </summary>
         [Name("Auditor")]
-        [Description("Auditor Description")]
+        [Description("The Auditor role allows the holder to check the role history of members of the corporation. ")]
         Auditor = 1 << 12,
 
         TakeFromDivision1Hangar = (ulong)1 << 13,
