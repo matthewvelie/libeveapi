c87704516309b3e61fca7e162722843c73225ffe
matthewvelie <matthewvelie@0af07104-5242-0410-b30e-b11728ae724a>
2008-01-15 05:23:06 +0000

    Updated unit tests

--- trunk/UnitTests/MarketOrdersTests.cs
+++ trunk/UnitTests/MarketOrdersTests.cs
@@ -19,19 +19,41 @@ namespace UnitTests
         [Test]
         public void GetCharMarketOrdersTest()
         {
+            MarketOrder marketOrder = EveApi.GetMarketOrderList(MarketOrderType.Character, "userId", "charId", "apiKey");   
 
+            //Assert.AreEqual(1, accountBalance.AccountBalanceItems.Length);
+            //Assert.AreEqual("4807144", accountBalance.AccountBalanceItems[0].AccountId);
+            //Assert.AreEqual("1000", accountBalance.AccountBalanceItems[0].AccountKey);
+            //Assert.AreEqual(209127823.31, accountBalance.AccountBalanceItems[0].Balance);
         }
 
         [Test]
         public void GetCorpMarketOrdersTest()
         {
-
+            MarketOrder marketOrder = EveApi.GetMarketOrderList(MarketOrderType.Corporation, "userId", "charId", "apiKey");
         }
 
         [Test]
         public void MarketOrderPersist()
         {
+            ResponseCache.Clear();
 
+            MarketOrder marketOrder = EveApi.GetMarketOrderList(MarketOrderType.Corporation, "userId", "charId", "apiKey");
+            ResponseCache.Save("ResponseCache.xml");
+            ResponseCache.Clear();
+            ResponseCache.Load("ResponseCache.xml");
+            MarketOrder cachedmarketOrder = EveApi.GetMarketOrderList(MarketOrderType.Corporation, "userId", "charId", "apiKey");
+
+            Assert.AreEqual(marketOrder.CachedUntilLocal, cachedmarketOrder.CachedUntilLocal);
+
+
+            for (int i = 0; i < marketOrder.MarketOrderItems.Length; i++)
+            {
+                Assert.AreEqual(marketOrder.MarketOrderItems[i].accountKey, cachedmarketOrder.MarketOrderItems[i].accountKey);
+                Assert.AreEqual(marketOrder.MarketOrderItems[i].charID, cachedmarketOrder.MarketOrderItems[i].charID);
+                Assert.AreEqual(marketOrder.MarketOrderItems[i].minVolume, cachedmarketOrder.MarketOrderItems[i].minVolume);
+            }
+            
         }
     }
 }
--- trunk/UnitTests/WalletTransactionsTests.cs
+++ trunk/UnitTests/WalletTransactionsTests.cs
@@ -31,7 +31,24 @@ namespace UnitTests
         [Test]
         public void WalletTransactionsPersist()
         {
+            ResponseCache.Clear();
 
+            WalletTransactions marketTransactions = EveApi.GetMarketTransactionsList(WalletTransactionListType.Corporation, "userId", "charId", "apiKey");
+            ResponseCache.Save("ResponseCache.xml");
+            ResponseCache.Clear();
+            ResponseCache.Load("ResponseCache.xml");
+            WalletTransactions cachedmarketTransactions = EveApi.GetMarketTransactionsList(WalletTransactionListType.Corporation, "userId", "charId", "apiKey");
+
+            Assert.AreEqual(marketTransactions.CachedUntilLocal, cachedmarketTransactions.CachedUntilLocal);
+
+            /*
+            for (int i = 0; i < accountBalance.AccountBalanceItems.Length; i++)
+            {
+                Assert.AreEqual(accountBalance.AccountBalanceItems[i].AccountId, cachedAccountBalance.AccountBalanceItems[i].AccountId);
+                Assert.AreEqual(accountBalance.AccountBalanceItems[i].AccountKey, cachedAccountBalance.AccountBalanceItems[i].AccountKey);
+                Assert.AreEqual(accountBalance.AccountBalanceItems[i].Balance, cachedAccountBalance.AccountBalanceItems[i].Balance);
+            }
+             */
         }
     }
 }
