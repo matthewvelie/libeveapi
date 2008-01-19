6fcde529ecc09b5287727b9fe5424cff4d92d8bf
lewisvance@gmail.com <lewisvance@gmail.com@0af07104-5242-0410-b30e-b11728ae724a>
2008-01-19 23:42:34 +0000

    renamed, made child class and example

--- trunk/libeveapi/ResponseObjects/ConquerableStation.cs
+++ trunk/libeveapi/ResponseObjects/ConquerableStation.cs
@@ -13,7 +13,7 @@ namespace libeveapi
         /// <summary>
         /// 
         /// </summary>
-        public ConquerableStationItem[] conquerableStationList = new ConquerableStationItem[0];
+        public ConquerableStation[] ConquerableStations = new ConquerableStation[0];
 
         /// <summary>
         /// Create an Conquerable Station list by parsing an XmlDocument response from the eveapi
@@ -25,70 +25,68 @@ namespace libeveapi
             ConquerableStationList stationList = new ConquerableStationList();
             stationList.ParseCommonElements(xmlDoc);
 
-            List<ConquerableStationItem> stationItemList = new List<ConquerableStationItem>();
+            List<ConquerableStation> stationItemList = new List<ConquerableStation>();
             foreach (XmlNode node in xmlDoc.SelectNodes("//rowset[@name='outposts']/row"))
             {
                 stationItemList.Add(ParseStationRow(node));
             }
-            stationList.conquerableStationList = stationItemList.ToArray();
+            stationList.ConquerableStations = stationItemList.ToArray();
 
             return stationList;
         }
 
         /// <summary>
         /// Create an ConquerableStationItem by parsing a single row
-        /// Recursively parses all contained assets
         /// </summary>
         /// <param name="stationRow"></param>
         /// <returns></returns>
-        protected static ConquerableStationItem ParseStationRow(XmlNode stationRow)
+        protected static ConquerableStation ParseStationRow(XmlNode stationRow)
         {
-            ConquerableStationItem conquerableStationItem = new ConquerableStationItem();
-            conquerableStationItem.stationId = Convert.ToInt32(stationRow.Attributes["stationID"].InnerText);
-            conquerableStationItem.stationName = stationRow.Attributes["stationName"].InnerText;
-            conquerableStationItem.stationTypeId = Convert.ToInt32(stationRow.Attributes["stationTypeID"].InnerText);
-            conquerableStationItem.solarSystemId = Convert.ToInt32(stationRow.Attributes["solarSystemID"].InnerText);
-            conquerableStationItem.corporationId = Convert.ToInt32(stationRow.Attributes["corporationID"].InnerText);
-            conquerableStationItem.corporationName = stationRow.Attributes["corporationName"].InnerText;
+            ConquerableStation conquerableStationItem = new ConquerableStation();
+            conquerableStationItem.StationId = Convert.ToInt32(stationRow.Attributes["stationID"].InnerText);
+            conquerableStationItem.StationName = stationRow.Attributes["stationName"].InnerText;
+            conquerableStationItem.StationTypeId = Convert.ToInt32(stationRow.Attributes["stationTypeID"].InnerText);
+            conquerableStationItem.SolarSystemId = Convert.ToInt32(stationRow.Attributes["solarSystemID"].InnerText);
+            conquerableStationItem.CorporationId = Convert.ToInt32(stationRow.Attributes["corporationID"].InnerText);
+            conquerableStationItem.CorporationName = stationRow.Attributes["corporationName"].InnerText;
 
             return conquerableStationItem;
         }
+
+        /// <summary>
+        /// Represents one conquerable station / outpost
+        /// </summary>
+        public class ConquerableStation
+        {
+            /// <summary>
+            /// The unique station Id
+            /// </summary>
+            public int StationId;
+
+            /// <summary>
+            /// The name of the station (corp gets to name it)
+            /// </summary>
+            public string StationName;
+
+            /// <summary>
+            /// The typeId of the station / outpost
+            /// </summary>
+            public int StationTypeId;
+
+            /// <summary>
+            /// The solarSystemId that the station / outpost is located in
+            /// </summary>
+            public int SolarSystemId;
+
+            /// <summary>
+            /// The id of the corporation that owns teh station / outpost
+            /// </summary>
+            public int CorporationId;
+
+            /// <summary>
+            /// The name of the corporation that owns the station / outpost
+            /// </summary>
+            public string CorporationName;
+        }
     }
-
-    /// <summary>
-    /// Represents one conquerable station / outpost
-    /// </summary>
-    public class ConquerableStationItem
-    {
-        /// <summary>
-        /// The unique station Id
-        /// </summary>
-        public int stationId;
-
-        /// <summary>
-        /// The name of the station (corp gets to name it)
-        /// </summary>
-        public string stationName;
-
-        /// <summary>
-        /// The typeId of the station / outpost
-        /// </summary>
-        public int stationTypeId;
-
-        /// <summary>
-        /// The solarSystemId that the station / outpost is located in
-        /// </summary>
-        public int solarSystemId;
-
-        /// <summary>
-        /// The id of the corporation that owns teh station / outpost
-        /// </summary>
-        public int corporationId;
-
-        /// <summary>
-        /// The name of the corporation that owns the station / outpost
-        /// </summary>
-        public string corporationName;
-    }
-
 }
