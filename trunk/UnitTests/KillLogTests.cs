69ad03b9caea808828233cae185d291b8d3c3d03
lewisvance@gmail.com <lewisvance@gmail.com@0af07104-5242-0410-b30e-b11728ae724a>
2008-01-20 09:09:32 +0000

    comments and such

--- trunk/UnitTests/KillLogTests.cs
+++ trunk/UnitTests/KillLogTests.cs
@@ -25,45 +25,45 @@ namespace UnitTests
             KillLog.Kill secondKill = getKill(62, killLog.Kills);
             
             //Victim Checks
-            Assert.AreEqual(150340823, firstKill.Victim.CharacterID);
+            Assert.AreEqual(150340823, firstKill.Victim.CharacterId);
             Assert.AreEqual("Dieinafire", firstKill.Victim.CharacterName);
-            Assert.AreEqual(1000169, firstKill.Victim.CorporationID);
+            Assert.AreEqual(1000169, firstKill.Victim.CorporationId);
             Assert.AreEqual("Center for Advanced Studies", firstKill.Victim.CorporationName);
-            Assert.AreEqual(0, firstKill.Victim.AllianceID);
+            Assert.AreEqual(0, firstKill.Victim.AllianceId);
             Assert.AreEqual(6378, firstKill.Victim.DamageTaken);
-            Assert.AreEqual(12003, firstKill.Victim.ShipTypeID);
+            Assert.AreEqual(12003, firstKill.Victim.ShipTypeId);
             Assert.AreEqual("", firstKill.Victim.AllianceName);
 
-            Assert.AreEqual(150340823, secondKill.Victim.CharacterID);
+            Assert.AreEqual(150340823, secondKill.Victim.CharacterId);
             Assert.AreEqual("Dieinafire", secondKill.Victim.CharacterName);
-            Assert.AreEqual(1000169, secondKill.Victim.CorporationID);
+            Assert.AreEqual(1000169, secondKill.Victim.CorporationId);
             Assert.AreEqual("Center for Advanced Studies", secondKill.Victim.CorporationName);
-            Assert.AreEqual(0, secondKill.Victim.AllianceID);
+            Assert.AreEqual(0, secondKill.Victim.AllianceId);
             Assert.AreEqual(455, secondKill.Victim.DamageTaken);
-            Assert.AreEqual(606, secondKill.Victim.ShipTypeID);
+            Assert.AreEqual(606, secondKill.Victim.ShipTypeId);
             Assert.AreEqual("", secondKill.Victim.AllianceName);
 
             //Attackers Check
-            Assert.AreEqual(0, firstKill.Attackers[0].CharacterID);
+            Assert.AreEqual(0, firstKill.Attackers[0].CharacterId);
             Assert.AreEqual("", firstKill.Attackers[0].CharacterName);
-            Assert.AreEqual(1000127, firstKill.Attackers[0].CorporationID);
+            Assert.AreEqual(1000127, firstKill.Attackers[0].CorporationId);
             Assert.AreEqual("Guristas", firstKill.Attackers[0].CorporationName);
-            Assert.AreEqual(0, firstKill.Attackers[0].AllianceID);
+            Assert.AreEqual(0, firstKill.Attackers[0].AllianceId);
             Assert.AreEqual(6313, firstKill.Attackers[0].DamageDone);
-            Assert.AreEqual(203, firstKill.Attackers[0].ShipTypeID);
-            Assert.AreEqual(0, firstKill.Attackers[0].WeaponTypeID);
+            Assert.AreEqual(203, firstKill.Attackers[0].ShipTypeId);
+            Assert.AreEqual(0, firstKill.Attackers[0].WeaponTypeId);
             Assert.AreEqual(true, firstKill.Attackers[0].FinalBlow);
             Assert.AreEqual(0, firstKill.Attackers[0].SecurityStatus);
             Assert.AreEqual("", firstKill.Attackers[0].AllianceName);
 
-            Assert.AreEqual(150131146, secondKill.Attackers[1].CharacterID);
+            Assert.AreEqual(150131146, secondKill.Attackers[1].CharacterId);
             Assert.AreEqual("Mark Player", secondKill.Attackers[1].CharacterName);
-            Assert.AreEqual(150147571, secondKill.Attackers[1].CorporationID);
+            Assert.AreEqual(150147571, secondKill.Attackers[1].CorporationId);
             Assert.AreEqual("Peanut Butter Jelly Time", secondKill.Attackers[1].CorporationName);
-            Assert.AreEqual(150148475, secondKill.Attackers[1].AllianceID);
+            Assert.AreEqual(150148475, secondKill.Attackers[1].AllianceId);
             Assert.AreEqual(0, secondKill.Attackers[1].DamageDone);
-            Assert.AreEqual(24698, secondKill.Attackers[1].ShipTypeID);
-            Assert.AreEqual(25715, secondKill.Attackers[1].WeaponTypeID);
+            Assert.AreEqual(24698, secondKill.Attackers[1].ShipTypeId);
+            Assert.AreEqual(25715, secondKill.Attackers[1].WeaponTypeId);
             Assert.AreEqual(false, secondKill.Attackers[1].FinalBlow);
             Assert.AreEqual((float)0.3, secondKill.Attackers[1].SecurityStatus);
             Assert.AreEqual("Margaritaville", secondKill.Attackers[1].AllianceName);
@@ -73,12 +73,12 @@ namespace UnitTests
 
             Assert.AreEqual(6, secondKill.Items.Length);
             Assert.AreEqual(3, secondKill.Items[1].ContainedItems.Length);
-            Assert.AreEqual(3520, secondKill.Items[0].TypeID);
+            Assert.AreEqual(3520, secondKill.Items[0].TypeId);
             Assert.AreEqual(InventoryFlagType.FlagNone, secondKill.Items[0].Flag);
             Assert.AreEqual(3, secondKill.Items[0].QtyDropped);
             Assert.AreEqual(1, secondKill.Items[0].QtyDestroyed);
 
-            Assert.AreEqual(1236, secondKill.Items[1].ContainedItems[1].TypeID);
+            Assert.AreEqual(1236, secondKill.Items[1].ContainedItems[1].TypeId);
             Assert.AreEqual(InventoryFlagType.FlagNone, secondKill.Items[1].ContainedItems[1].Flag);
             Assert.AreEqual(2, secondKill.Items[1].ContainedItems[1].QtyDropped);
             Assert.AreEqual(1, secondKill.Items[1].ContainedItems[1].QtyDestroyed);
@@ -99,14 +99,14 @@ namespace UnitTests
             KillLog.Kill firstKill = getKill(63, killLog.Kills);
             KillLog.Kill cachedFirstKill = getKill(63, cachedKillLog.Kills);
 
-            Assert.AreEqual(firstKill.Victim.CharacterID, cachedFirstKill.Victim.CharacterID);
+            Assert.AreEqual(firstKill.Victim.CharacterId, cachedFirstKill.Victim.CharacterId);
         }
 
         private KillLog.Kill getKill(int killID, KillLog.Kill[] kills)
         {
             foreach (KillLog.Kill kill in kills)
             {
-                if (kill.KillID == killID)
+                if (kill.KillId == killID)
                     return kill;
             }
             return null;
@@ -114,7 +114,7 @@ namespace UnitTests
 
         private bool CompareItems(KillLog.Item item1, KillLog.Item item2)
         {
-            if (item1.TypeID != item2.TypeID)
+            if (item1.TypeId != item2.TypeId)
             {
                 return false;
             }
--- trunk/libeveapi/ResponseObjects/KillLog.cs
+++ trunk/libeveapi/ResponseObjects/KillLog.cs
@@ -32,38 +32,39 @@ namespace libeveapi
                 Kill tmpKill = new Kill();
                 
                 //Kill Info
-                tmpKill.KillID = Convert.ToInt32(Kill.Attributes["killID"].InnerText);
-                tmpKill.SolarSystemID = Convert.ToInt32(Kill.Attributes["solarSystemID"].InnerText);
-                tmpKill.KillTime = Convert.ToDateTime(Kill.Attributes["killTime"].InnerText);
-                tmpKill.MoonID = Convert.ToInt32(Kill.Attributes["moonID"].InnerText);
+                tmpKill.KillId = Convert.ToInt32(Kill.Attributes["killID"].InnerText);
+                tmpKill.SolarSystemId = Convert.ToInt32(Kill.Attributes["solarSystemID"].InnerText);
+                tmpKill.KillTime = TimeUtilities.ConvertCCPTimeStringToDateTimeUTC(Kill.Attributes["killTime"].InnerText);
+                tmpKill.KillTimeLocal = TimeUtilities.ConvertCCPToLocalTime(tmpKill.KillTime);
+                tmpKill.MoonId = Convert.ToInt32(Kill.Attributes["moonID"].InnerText);
                 
                 //Victim
                 XmlNode victimNode = Kill.SelectSingleNode("./victim");
-                tmpKill.Victim.CharacterID = Convert.ToInt32(victimNode.Attributes["characterID"].InnerText);
+                tmpKill.Victim.CharacterId = Convert.ToInt32(victimNode.Attributes["characterID"].InnerText);
                 tmpKill.Victim.CharacterName = victimNode.Attributes["characterName"].InnerText;
-                tmpKill.Victim.CorporationID = Convert.ToInt32(victimNode.Attributes["corporationID"].InnerText);
+                tmpKill.Victim.CorporationId = Convert.ToInt32(victimNode.Attributes["corporationID"].InnerText);
                 tmpKill.Victim.CorporationName = victimNode.Attributes["corporationName"].InnerText;
-                tmpKill.Victim.AllianceID = Convert.ToInt32(victimNode.Attributes["allianceID"].InnerText);
+                tmpKill.Victim.AllianceId = Convert.ToInt32(victimNode.Attributes["allianceID"].InnerText);
                 tmpKill.Victim.AllianceName = (victimNode.Attributes.GetNamedItem("allianceName") != null) ? victimNode.Attributes["allianceName"].InnerText : "";
                 tmpKill.Victim.DamageTaken = Convert.ToInt32(victimNode.Attributes["damageTaken"].InnerText);
-                tmpKill.Victim.ShipTypeID = Convert.ToInt32(victimNode.Attributes["shipTypeID"].InnerText);
+                tmpKill.Victim.ShipTypeId = Convert.ToInt32(victimNode.Attributes["shipTypeID"].InnerText);
 
                 //Atackers
                 List<Attacker> attackerList = new List<Attacker>();
                 foreach (XmlNode attacker in Kill.SelectNodes("./rowset[@name='attackers']/row"))
                 {
                     Attacker tmpAttacker = new Attacker();
-                    tmpAttacker.CharacterID = Convert.ToInt32(attacker.Attributes["characterID"].InnerText);
+                    tmpAttacker.CharacterId = Convert.ToInt32(attacker.Attributes["characterID"].InnerText);
                     tmpAttacker.CharacterName = attacker.Attributes["characterName"].InnerText;
-                    tmpAttacker.CorporationID = Convert.ToInt32(attacker.Attributes["corporationID"].InnerText);
+                    tmpAttacker.CorporationId = Convert.ToInt32(attacker.Attributes["corporationID"].InnerText);
                     tmpAttacker.CorporationName = attacker.Attributes["corporationName"].InnerText;
-                    tmpAttacker.AllianceID = Convert.ToInt32(attacker.Attributes["allianceID"].InnerText);
+                    tmpAttacker.AllianceId = Convert.ToInt32(attacker.Attributes["allianceID"].InnerText);
                     tmpAttacker.AllianceName = attacker.Attributes["allianceName"].InnerText;
                     tmpAttacker.SecurityStatus = (float)Convert.ToDouble(attacker.Attributes["securityStatus"].InnerText);
                     tmpAttacker.DamageDone = Convert.ToInt32(attacker.Attributes["damageDone"].InnerText);
                     tmpAttacker.FinalBlow = Convert.ToBoolean(Convert.ToInt32(attacker.Attributes["finalBlow"].InnerText));
-                    tmpAttacker.WeaponTypeID = Convert.ToInt32(attacker.Attributes["weaponTypeID"].InnerText);
-                    tmpAttacker.ShipTypeID = Convert.ToInt32(attacker.Attributes["shipTypeID"].InnerText);
+                    tmpAttacker.WeaponTypeId = Convert.ToInt32(attacker.Attributes["weaponTypeID"].InnerText);
+                    tmpAttacker.ShipTypeId = Convert.ToInt32(attacker.Attributes["shipTypeID"].InnerText);
                     attackerList.Add(tmpAttacker);
                 }
                 tmpKill.Attackers = attackerList.ToArray();
@@ -91,7 +92,7 @@ namespace libeveapi
         protected static Item ParseItemRow(XmlNode ItemNode)
         {
             Item tmpItem = new Item();
-            tmpItem.TypeID = Convert.ToInt32(ItemNode.Attributes["typeID"].InnerText);
+            tmpItem.TypeId = Convert.ToInt32(ItemNode.Attributes["typeID"].InnerText);
             tmpItem.QtyDropped = Convert.ToInt32(ItemNode.Attributes["qtyDropped"].InnerText);
             tmpItem.QtyDestroyed = Convert.ToInt32(ItemNode.Attributes["qtyDestroyed"].InnerText);
 
@@ -123,12 +124,49 @@ namespace libeveapi
         /// </summary>
         public class Kill
         {
-            public int KillID;
-            public int SolarSystemID;
+            /// <summary>
+            /// Unique kill ID for this kill. This is to be used as the input for 
+            /// beforeKillID if you need to scroll back. This is globally unique 
+            /// and can be used for uniquely identifying a kill to other killboards.
+            /// </summary>
+            public int KillId;
+
+            /// <summary>
+            /// The ID of the solar system this kill occurred in.
+            /// </summary>
+            public int SolarSystemId;
+
+            /// <summary>
+            /// What time the event occurred.
+            /// </summary>
             public DateTime KillTime;
-            public int MoonID;
+
+            /// <summary>
+            /// What time the event occurred in local time.
+            /// </summary>
+            public DateTime KillTimeLocal;
+
+            /// <summary>
+            /// In some situations, this is present to define what moon a 
+            /// kill occurred at. Note that this is generally only present 
+            /// in situations where the loss is a POS structure. It is not
+            /// guaranteed to be populated.
+            /// </summary>
+            public int MoonId;
+
+            /// <summary>
+            /// The victim of the attack
+            /// </summary>
             public VictimPilot Victim = new VictimPilot();
+
+            /// <summary>
+            /// The attacking pilots
+            /// </summary>
             public Attacker[] Attackers = new Attacker[0];
+
+            /// <summary>
+            /// The items lost / dropped in the attack
+            /// </summary>
             public Item[] Items = new Item[0];
         }
 
@@ -137,19 +175,60 @@ namespace libeveapi
         /// </summary>
         public class Pilot
         {
-            public int CharacterID;
+            /// <summary>
+            /// Character's ID. Fairly self-explanatory. Can be 0 in which 
+            /// case this kill was not done by a character and instead 
+            /// was done by a corporation. In the victim case, this implies 
+            /// it is a structure loss.
+            /// </summary>
+            public int CharacterId;
+
+            /// <summary>
+            /// If present, the name of the above characterID.
+            /// </summary>
             public string CharacterName;
-            public int CorporationID;
+
+            /// <summary>
+            /// The ID of the corporation. This will always be present, 
+            /// as there is always a corporation behind the victim, 
+            /// whether it is the corporation itself or simply someone 
+            /// in that corporation.
+            /// </summary>
+            public int CorporationId;
+
+            /// <summary>
+            /// The name of the corporation associated with the kill.
+            /// </summary>
             public string CorporationName;
-            public int AllianceID;
+
+            /// <summary>
+            /// If not 0, this is the ID of the alliance the corporation belongs to.
+            /// </summary>
+            public int AllianceId;
+
+            /// <summary>
+            /// The name of the alliance associated with the kill.
+            /// </summary>
             public string AllianceName;
-            public int ShipTypeID;
+
+            /// <summary>
+            /// The item lost. This could be a ship as suggested by the name 
+            /// but can potentially be anything that generates a kill event.
+            /// </summary>
+            public int ShipTypeId;
         }
         /// <summary>
         /// The victim pilot
         /// </summary>
         public class VictimPilot : Pilot
         {
+            /// <summary>
+            /// How much damage the victim took before 
+            /// succumbing to fiery defeat and humiliation.
+            /// Please note that this damage is calculated after resists. 
+            /// It does give you a decent idea of how much 
+            /// they were tanking, however.
+            /// </summary>
             public int DamageTaken;
         }
 
@@ -158,10 +237,26 @@ namespace libeveapi
         /// </summary>
         public class Attacker : Pilot
         {
-            public float SecurityStatus;
+            /// <summary>
+            /// The security status of the aggressor at the time of this kill.
+            /// </summary>
+            public double SecurityStatus;
+
+            /// <summary>
+            /// The amount of damage done to the victim. This is post-resists damage done.
+            /// </summary>
             public int DamageDone;
+
+            /// <summary>
+            /// Whether or not this aggressor is attributed with the so-called "final blow."
+            /// </summary>
             public bool FinalBlow;
-            public int WeaponTypeID;
+
+            /// <summary>
+            /// What weapon we decided to show this person as using. Often a weapon, 
+            /// sometimes a ship or missile, rarely a fish
+            /// </summary>
+            public int WeaponTypeId;
         }
 
         /// <summary>
@@ -169,11 +264,38 @@ namespace libeveapi
         /// </summary>
         public class Item
         {
-            public int TypeID;
+            /// <summary>
+            /// The typeID of this item, references the invTypes table
+            /// </summary>
+            public int TypeId;
+
+            /// <summary>
+            /// See <see cref="InventoryFlagType" /> for full details.
+            /// </summary>
             public InventoryFlagType Flag;
+
+            /// <summary>
+            /// How many of this item were dropped. 
+            /// If the user had multiple stacks, we compress the total number of 
+            /// dropped items to just one stack for space purposes
+            /// </summary>
             public int QtyDropped;
+
+            /// <summary>
+            /// How many of this item fell victim to atomic
+            /// dispersal and other sad little ends that such 
+            /// things can meet.
+            /// </summary>
             public int QtyDestroyed;
+
+            /// <summary>
+            /// True if the destroyed item contained other items.
+            /// </summary>
             public bool Container;
+
+            /// <summary>
+            /// List of contained items.
+            /// </summary>
             public Item[] ContainedItems = new Item[0];
         }
     }
