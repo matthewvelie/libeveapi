aab2d10d8675f7c2437a340f6a2477b146529694
lewisvance@gmail.com <lewisvance@gmail.com@0af07104-5242-0410-b30e-b11728ae724a>
2008-01-20 19:43:42 +0000

    updated for docs

--- trunk/libeveapi/ResponseObjects/MarketOrders.cs
+++ trunk/libeveapi/ResponseObjects/MarketOrders.cs
@@ -96,154 +96,154 @@ namespace libeveapi
 
             return marketItem;
         }
-    }
-
-    /// <summary>
-    /// A single market order associated with a person or corporation
-    /// </summary>
-    public class MarketOrderItem
-    {
-        /// <summary>
-        /// Order type (Buy or Sell)
-        /// </summary>
-        public MarketOrderType OrderType;
 
         /// <summary>
-        /// Order id, not forever unique but for this pull they will be unique
+        /// A single market order associated with a person or corporation
         /// </summary>
-        public int OrderId;
+        public class MarketOrderItem
+        {
+            /// <summary>
+            /// Order type (Buy or Sell)
+            /// </summary>
+            public MarketOrderType OrderType;
+
+            /// <summary>
+            /// Order id, not forever unique but for this pull they will be unique
+            /// </summary>
+            public int OrderId;
+
+            /// <summary>
+            /// Character Id of the character who placed the market order
+            /// </summary>
+            public long CharId;
+
+            /// <summary>
+            /// The Id of the station that the order was placed in
+            /// </summary>
+            public long StationId;
+
+            /// <summary>
+            /// The quantity of the items required/offered when the order was placed
+            /// </summary>
+            public long VolEntered;
+
+            /// <summary>
+            /// The quantitiy of items that are still for sale/ still required
+            /// </summary>
+            public long VolRemaining;
+
+            /// <summary>
+            /// For bids (buy orders) the minimum quantity that must be sold in one
+            /// sale so that the order is accepted.
+            /// </summary>
+            public long MinVolume;
+
+            /// <summary>
+            /// See <see cref="marketOrderState"/> for full descriptions of each order state
+            /// </summary>
+            public MarketOrderState OrderState;
+
+            /// <summary>
+            /// This is the typeId of the item that is being bought/sold
+            /// </summary>
+            public long TypeId;
+
+            /// <summary>
+            /// This is the range of the order
+            /// For sell orders it is always 32767
+            /// For buy orders it is either -1 = station, 0 = solar system
+            /// Any number above 1 is number of jumps in region
+            /// And 32767 means region
+            /// </summary>
+            public int Range;
+
+            /// <summary>
+            /// This is which wallet the order is using, for a personal order
+            /// this will always be 1000, for corporation orders it can be 1000-1006
+            /// depending on which wallet is being used
+            /// </summary>
+            public int AccountKey;
+
+            /// <summary>
+            /// How many days this order is good for. Expiration is issued + duration in days
+            /// </summary>
+            public int Duration;
+
+            /// <summary>
+            /// How much ISK is in escrow. Valid for buy orders only (I believe).
+            /// </summary>
+            public double Escrow;
+
+            /// <summary>
+            /// The cost per unit for this order
+            /// </summary>
+            public double Price;
+
+            /// <summary>
+            /// If true this is a bid or buy order, else it is a sell order
+            /// </summary>
+            public bool Bid;
+
+            /// <summary>
+            /// This is when the order was issued
+            /// </summary>
+            public DateTime Issued;
+
+            /// <summary>
+            /// This is when the order was issued in local time
+            /// </summary>
+            public DateTime IssuedLocal;
+        }
 
         /// <summary>
-        /// Character Id of the character who placed the market order
+        /// This is the current state of the market order, letting you know if the
+        /// item is still on the market or not.
         /// </summary>
-        public long CharId;
+        public enum MarketOrderState
+        {
+            /// <summary>
+            /// If the market order is still active and up on the market
+            /// </summary>
+            OpenActive = 0,
 
-        /// <summary>
-        /// The Id of the station that the order was placed in
-        /// </summary>
-        public long StationId;
+            /// <summary>
+            /// The order has been closed
+            /// </summary>
+            Closed = 1,
 
-        /// <summary>
-        /// The quantity of the items required/offered when the order was placed
-        /// </summary>
-        public long VolEntered;
+            /// <summary>
+            /// The order has expired, or has been fufilled so it is no longer active
+            /// </summary>
+            ExpiredFulfilled = 2,
 
-        /// <summary>
-        /// The quantitiy of items that are still for sale/ still required
-        /// </summary>
-        public long VolRemaining;
+            /// <summary>
+            /// The order was canceled
+            /// </summary>
+            Canceled = 3,
 
-        /// <summary>
-        /// For bids (buy orders) the minimum quantity that must be sold in one
-        /// sale so that the order is accepted.
-        /// </summary>
-        public long MinVolume;
+            /// <summary>
+            /// The order is currently pending, and not on the market
+            /// </summary>
+            Pending = 4,
 
-        /// <summary>
-        /// See <see cref="marketOrderState"/> for full descriptions of each order state
-        /// </summary>
-        public MarketOrderState OrderState;
+            /// <summary>
+            /// The character that this order was associated with has been deleted
+            /// </summary>
+            CharacterDeleted = 5
+        }
 
-        /// <summary>
-        /// This is the typeId of the item that is being bought/sold
-        /// </summary>
-        public long TypeId;
+        public enum MarketOrderType
+        {
+            /// <summary>
+            /// Denotes a buy order
+            /// </summary>
+            Buy,
 
-        /// <summary>
-        /// This is the range of the order
-        /// For sell orders it is always 32767
-        /// For buy orders it is either -1 = station, 0 = solar system
-        /// Any number above 1 is number of jumps in region
-        /// And 32767 means region
-        /// </summary>
-        public int Range;
-
-        /// <summary>
-        /// This is which wallet the order is using, for a personal order
-        /// this will always be 1000, for corporation orders it can be 1000-1006
-        /// depending on which wallet is being used
-        /// </summary>
-        public int AccountKey;
-
-        /// <summary>
-        /// How many days this order is good for. Expiration is issued + duration in days
-        /// </summary>
-        public int Duration;
-
-        /// <summary>
-        /// How much ISK is in escrow. Valid for buy orders only (I believe).
-        /// </summary>
-        public double Escrow;
-
-        /// <summary>
-        /// The cost per unit for this order
-        /// </summary>
-        public double Price;
-
-        /// <summary>
-        /// If true this is a bid or buy order, else it is a sell order
-        /// </summary>
-        public bool Bid;
-
-        /// <summary>
-        /// This is when the order was issued
-        /// </summary>
-        public DateTime Issued;
-
-        /// <summary>
-        /// This is when the order was issued in local time
-        /// </summary>
-        public DateTime IssuedLocal;
-    }
-
-    /// <summary>
-    /// This is the current state of the market order, letting you know if the
-    /// item is still on the market or not.
-    /// </summary>
-    public enum MarketOrderState
-    {
-        /// <summary>
-        /// If the market order is still active and up on the market
-        /// </summary>
-        OpenActive = 0,
-
-        /// <summary>
-        /// The order has been closed
-        /// </summary>
-        Closed = 1,
-
-        /// <summary>
-        /// The order has expired, or has been fufilled so it is no longer active
-        /// </summary>
-        ExpiredFulfilled = 2,
-
-        /// <summary>
-        /// The order was canceled
-        /// </summary>
-        Canceled = 3,
-
-        /// <summary>
-        /// The order is currently pending, and not on the market
-        /// </summary>
-        Pending = 4,
-
-        /// <summary>
-        /// The character that this order was associated with has been deleted
-        /// </summary>
-        CharacterDeleted = 5
-    }
-
-    public enum MarketOrderType
-    {
-        /// <summary>
-        /// Denotes a buy order
-        /// </summary>
-        Buy,
-
-        /// <summary>
-        /// Denotes a sell order
-        /// </summary>
-        Sell
+            /// <summary>
+            /// Denotes a sell order
+            /// </summary>
+            Sell
+        }
     }
 
     /// <summary>
