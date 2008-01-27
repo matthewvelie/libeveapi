bb8b1a109030850197958294bd1814d0b38a5ead
lewisvance@gmail.com <lewisvance@gmail.com@0af07104-5242-0410-b30e-b11728ae724a>
2008-01-27 05:02:56 +0000

    added properties

--- trunk/libeveapi/ResponseObjects/ConquerableStation.cs
+++ trunk/libeveapi/ResponseObjects/ConquerableStation.cs
@@ -10,10 +10,16 @@ namespace libeveapi
     /// </summary>
     public class ConquerableStationList : ApiResponse
     {
+        private ConquerableStation[] conquerableStations = new ConquerableStation[0];
+
         /// <summary>
         /// 
         /// </summary>
-        public ConquerableStation[] ConquerableStations = new ConquerableStation[0];
+        public ConquerableStation[] ConquerableStations
+        {
+            get { return conquerableStations; }
+            set { conquerableStations = value; }
+        }
 
         /// <summary>
         /// Create an Conquerable Station list by parsing an XmlDocument response from the eveapi
@@ -58,35 +64,66 @@ namespace libeveapi
         /// </summary>
         public class ConquerableStation
         {
+            private int stationId;
+            private string stationName;
+            private int stationTypeId;
+            private int solarSystemId;
+            private int corporationId;
+            private string corporationName;
+
             /// <summary>
             /// The unique station Id
             /// </summary>
-            public int StationId;
+            public int StationId
+            {
+                get { return stationId; }
+                set { stationId = value; }
+            }
 
             /// <summary>
             /// The name of the station (corp gets to name it)
             /// </summary>
-            public string StationName;
+            public string StationName
+            {
+                get { return stationName; }
+                set { stationName = value; }
+            }
 
             /// <summary>
             /// The typeId of the station / outpost
             /// </summary>
-            public int StationTypeId;
+            public int StationTypeId
+            {
+                get { return stationTypeId; }
+                set { stationTypeId = value; }
+            }
 
             /// <summary>
             /// The solarSystemId that the station / outpost is located in
             /// </summary>
-            public int SolarSystemId;
+            public int SolarSystemId
+            {
+                get { return solarSystemId; }
+                set { solarSystemId = value; }
+            }
 
             /// <summary>
             /// The id of the corporation that owns teh station / outpost
             /// </summary>
-            public int CorporationId;
+            public int CorporationId
+            {
+                get { return corporationId; }
+                set { corporationId = value; }
+            }
 
             /// <summary>
             /// The name of the corporation that owns the station / outpost
             /// </summary>
-            public string CorporationName;
+            public string CorporationName
+            {
+                get { return corporationName; }
+                set { corporationName = value; }
+            }
         }
     }
 }
