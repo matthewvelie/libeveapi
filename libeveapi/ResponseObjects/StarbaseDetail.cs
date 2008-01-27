2455418e002e4c01ea08d35d1d260f2143b1536a
Lewis Vance <lewisvance@gmail.com>
2008-01-27 06:41:21 +0000

    added properties

--- libeveapi/ResponseObjects/StarbaseDetail.cs
+++ libeveapi/ResponseObjects/StarbaseDetail.cs
@@ -10,57 +10,118 @@ namespace libeveapi
     /// </summary>
     public class StarbaseDetail : ApiResponse
     {
+        private string usageFlags;
+        private string deployFlags;
+        private bool allowCorporationMembers;
+        private bool allowAllianceMembers;
+        private bool claimSovereignty;
+        private bool onStandingDropEnabled;
+        private string onStandingDropStanding;
+        private bool onStatusDropEnabled;
+        private string onStatusDropStanding;
+        private bool onAgressionEnabled;
+        private bool onCorporationWarEnabled;
+        private FuelListItem[] fuelList = new FuelListItem[0];
+
         /// <summary>
         /// 
         /// </summary>
-        public string UsageFlags;
+        public string UsageFlags
+        {
+            get { return usageFlags; }
+            set { usageFlags = value; }
+        }
         /// <summary>
         /// 
         /// </summary>
-        public string DeployFlags;
+        public string DeployFlags
+        {
+            get { return deployFlags; }
+            set { deployFlags = value; }
+        }
         /// <summary>
         /// Allow corporation members inside the force field
         /// </summary>
-        public bool AllowCorporationMembers;
+        public bool AllowCorporationMembers
+        {
+            get { return allowCorporationMembers; }
+            set { allowCorporationMembers = value; }
+        }
         /// <summary>
         /// Allow alliance members inside the force field
         /// </summary>
-        public bool AllowAllianceMembers;
+        public bool AllowAllianceMembers
+        {
+            get { return allowAllianceMembers; }
+            set { allowAllianceMembers = value; }
+        }
         /// <summary>
         /// Is the starbase claiming sovernty (only in 0.0 space)
         /// http://myeve.eve-online.com/devblog.asp?a=blog&bid=477
         /// </summary>
-        public bool ClaimSovereignty;
+        public bool ClaimSovereignty
+        {
+            get { return claimSovereignty; }
+            set { claimSovereignty = value; }
+        }
 
         /// <summary>
         /// Shoot on standing drop
         /// </summary>
-        public bool OnStandingDropEnabled;
+        public bool OnStandingDropEnabled
+        {
+            get { return onStandingDropEnabled; }
+            set { onStandingDropEnabled = value; }
+        }
         /// <summary>
         /// What target standing makes them a valid target
         /// </summary>
-        public string OnStandingDropStanding;
+        public string OnStandingDropStanding
+        {
+            get { return onStandingDropStanding; }
+            set { onStandingDropStanding = value; }
+        }
         /// <summary>
         /// Shoot on status drop
         /// </summary>
-        public bool OnStatusDropEnabled;
+        public bool OnStatusDropEnabled
+        {
+            get { return onStatusDropEnabled; }
+            set { onStatusDropEnabled = value; }
+        }
         /// <summary>
         /// What target security make them a valid target
         /// </summary>
-        public string OnStatusDropStanding;
+        public string OnStatusDropStanding
+        {
+            get { return onStatusDropStanding; }
+            set { onStatusDropStanding = value; }
+        }
         /// <summary>
         /// Shoot on target agression
         /// </summary>
-        public bool OnAgressionEnabled;
+        public bool OnAgressionEnabled
+        {
+            get { return onAgressionEnabled; }
+            set { onAgressionEnabled = value; }
+        }
         /// <summary>
         /// Shoot if at war with target
         /// </summary>
-        public bool OnCorporationWarEnabled;
+        public bool OnCorporationWarEnabled
+        {
+            get { return onCorporationWarEnabled; }
+            set { onCorporationWarEnabled = value; }
+        }
 
         /// <summary>
         /// 
         /// </summary>
-        public FuelListItem[] FuelList = new FuelListItem[0];
+        public FuelListItem[] FuelList
+        {
+            get { return fuelList; }
+            set { fuelList = value; }
+        }
 
         /// <summary>
         /// Create a StarbaseDetail by parsing an XmlDocument response from the eveapi
@@ -103,15 +164,26 @@ namespace libeveapi
         /// </summary>
         public class FuelListItem
         {
+            private string typeId;
+            private long quantity;
+
             /// <summary>
             /// Type Id of the fuel
             /// </summary>
-            public string TypeId;
+            public string TypeId
+            {
+                get { return typeId; }
+                set { typeId = value; }
+            }
 
             /// <summary>
             /// Number of units of the fuel remaining
             /// </summary>
-            public long Quantity;
+            public long Quantity
+            {
+                get { return quantity; }
+                set { quantity = value; }
+            }
         }
     }
 }
