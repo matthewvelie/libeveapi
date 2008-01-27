a1ddeba3aba60860d1092dcfa4926dc80c4b2f5b
Lewis Vance <lewisvance@gmail.com>
2008-01-27 04:37:03 +0000

    added properties to AllianceList

--- libeveapi/ResponseObjects/AllianceList.cs
+++ libeveapi/ResponseObjects/AllianceList.cs
@@ -7,10 +7,16 @@ namespace libeveapi
 {
     public class AllianceList : ApiResponse
     {
+        private AllianceListItem[] allianceListItems = new AllianceListItem[0];
+
         /// <summary>
         /// List of alliances
         /// </summary>
-        public AllianceListItem[] AllianceListItems = new AllianceListItem[0];
+        public AllianceListItem[] AllianceListItems
+        {
+            get { return allianceListItems; }
+            set { allianceListItems = value; }
+        }
 
         internal static AllianceList FromXmlDocument(XmlDocument xmlDoc)
         {
@@ -48,65 +54,128 @@ namespace libeveapi
             return allianceList;
         }
 
+        /// <summary>
+        /// Represents an alliance
+        /// </summary>
         public class AllianceListItem
         {
+            private string name;
+            private string shortName;
+            private int allianceId;
+            private int executorCorpId;
+            private int memberCount;
+            private DateTime startDate;
+            private DateTime startDateLocal;
+            private CorporationListItem[] corporationListItems = new CorporationListItem[0];
+
             /// <summary>
             /// full name of the alliance
             /// </summary>
-            public string Name;
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
 
             /// <summary>
             /// ticker name of the alliance
             /// </summary>
-            public string ShortName;
+            public string ShortName
+            {
+                get { return shortName; }
+                set { shortName = value; }
+            }
 
             /// <summary>
             /// unique identifier for this alliance
             /// </summary>
-            public int AllianceId;
+            public int AllianceId
+            {
+                get { return allianceId; }
+                set { allianceId = value; }
+            }
 
             /// <summary>
             /// unique identifier of executor corporation
             /// </summary>
-            public int ExecutorCorpId;
+            public int ExecutorCorpId
+            {
+                get { return executorCorpId; }
+                set { executorCorpId = value; }
+            }
 
             /// <summary>
             /// Current number of pilots in the alliance
             /// </summary>
-            public int MemberCount;
+            public int MemberCount
+            {
+                get { return memberCount; }
+                set { memberCount = value; }
+            }
 
             /// <summary>
             /// Date the alliance was created in CCP time
             /// </summary>
-            public DateTime StartDate;
+            public DateTime StartDate
+            {
+                get { return startDate; }
+                set { startDate = value; }
+            }
 
             /// <summary>
             /// Date the alliance was created in local time
             /// </summary>
-            public DateTime StartDateLocal;
+            public DateTime StartDateLocal
+            {
+                get { return startDateLocal; }
+                set { startDateLocal = value; }
+            }
 
             /// <summary>
             /// List of member corporations
             /// </summary>
-            public CorporationListItem[] CorporationListItems;
+            public CorporationListItem[] CorporationListItems
+            {
+                get { return corporationListItems; }
+                set { corporationListItems = value; }
+            }
         }
 
+        /// <summary>
+        /// Represents a member corporation
+        /// </summary>
         public class CorporationListItem
         {
+            private int corporationId;
+            private DateTime startDate;
+            private DateTime startDateLocal;
+
             /// <summary>
             /// unique identifier for the corporation
             /// </summary>
-            public int CorporationId;
+            public int CorporationId
+            {
+                get { return corporationId; }
+                set { corporationId = value; }
+            }
 
             /// <summary>
             /// date the corporation joined the alliance in CCP time
             /// </summary>
-            public DateTime StartDate;
+            public DateTime StartDate
+            {
+                get { return startDate; }
+                set { startDate = value; }
+            }
 
             /// <summary>
             /// date the corporation joined the alliance in local time
             /// </summary>
-            public DateTime StartDateLocal;
+            public DateTime StartDateLocal
+            {
+                get { return startDateLocal; }
+                set { startDateLocal = value; }
+            }
         }
     }
 }
