704512899baab0fc9bb9057dc31de8255759410c
lewisvance@gmail.com <lewisvance@gmail.com@0af07104-5242-0410-b30e-b11728ae724a>
2008-01-27 05:48:18 +0000

    added properties

--- trunk/libeveapi/ResponseObjects/JournalEntries.cs
+++ trunk/libeveapi/ResponseObjects/JournalEntries.cs
@@ -11,10 +11,16 @@ namespace libeveapi
     /// </summary>
     public class JournalEntries : ApiResponse
     {
+        private JournalEntryItem[] journalEntryItems = new JournalEntryItem[0];
+
         /// <summary>
         /// 
         /// </summary>
-        public JournalEntryItem[] JournalEntryItems = new JournalEntryItem[0];
+        public JournalEntryItem[] JournalEntryItems
+        {
+            get { return journalEntryItems; }
+            set { journalEntryItems = value; }
+        }
 
         /// <summary>
         /// Create an JournalEntryItemList by parsing an XmlDocument response from the eveapi
@@ -67,76 +73,142 @@ namespace libeveapi
         /// </summary>
         public class JournalEntryItem
         {
+            private DateTime date;
+            private DateTime dateLocal;
+            private int refId;
+            private int refTypeId;
+            private string ownerName1;
+            private int ownerId1;
+            private string ownerName2;
+            private int ownerId2;
+            private string argName1;
+            private int argId1;
+            private double amount;
+            private double balance;
+            private string reason;
+
             /// <summary>
             /// The date / time of the entry
             /// </summary>
-            public DateTime Date;
+            public DateTime Date
+            {
+                get { return date; }
+                set { date = value; }
+            }
 
             /// <summary>
             /// The date / time of the entry in local time
             /// </summary>
-            public DateTime DateLocal;
+            public DateTime DateLocal
+            {
+                get { return dateLocal; }
+                set { dateLocal = value; }
+            }
 
             /// <summary>
             /// A unique referenceId for each transaction
             /// </summary>
-            public int RefId;
+            public int RefId
+            {
+                get { return refId; }
+                set { refId = value; }
+            }
 
             /// <summary>
             /// The typeId of the item that is referenced in the entry
             /// </summary>
-            public int RefTypeId;
+            public int RefTypeId
+            {
+                get { return refTypeId; }
+                set { refTypeId = value; }
+            }
 
             /// <summary>
             /// Name of the person/corp giving money
             /// </summary>
-            public string OwnerName1;
+            public string OwnerName1
+            {
+                get { return ownerName1; }
+                set { ownerName1 = value; }
+            }
 
             /// <summary>
             /// TypeId/CharId of the person/corp giving money
             /// </summary>
-            public int OwnerId1;
+            public int OwnerId1
+            {
+                get { return ownerId1; }
+                set { ownerId1 = value; }
+            }
 
             /// <summary>
             /// Name of the person/corp recieving money
             /// </summary>
-            public string OwnerName2;
+            public string OwnerName2
+            {
+                get { return ownerName2; }
+                set { ownerName2 = value; }
+            }
 
             /// <summary>
             /// TypeId/CharId of the person/corp recieving money
             /// </summary>
-            public int OwnerId2;
+            public int OwnerId2
+            {
+                get { return ownerId2; }
+                set { ownerId2 = value; }
+            }
 
             /// <summary>
             /// This is either the system where the transaction took place
             /// For example when recieving bounties, or "EVE System" if used
             /// for other items like buying and selling
             /// </summary>
-            public string ArgName1;
+            public string ArgName1
+            {
+                get { return argName1; }
+                set { argName1 = value; }
+            }
 
             /// <summary>
             /// This is an argument Id that goes with the argument
             /// Use the data from: http://wiki.eve-dev.net/APIv2_Eve_RefTypes_XML
             /// to determine what this is used for
             /// </summary>
-            public int ArgId1;
+            public int ArgId1
+            {
+                get { return argId1; }
+                set { argId1 = value; }
+            }
 
             /// <summary>
             /// The amount of the transaction
             /// </summary>
-            public double Amount;
+            public double Amount
+            {
+                get { return amount; }
+                set { amount = value; }
+            }
 
             /// <summary>
             /// The balance left in the account after the transaction
             /// </summary>
-            public double Balance;
+            public double Balance
+            {
+                get { return balance; }
+                set { balance = value; }
+            }
 
             /// <summary>
             /// This is the note attached to the transaction if any
             /// If exists for bounties this is the list of people killed and how many
             /// Ex: TypeId:Number;TypeId:Number
             /// </summary>
-            public string Reason;
+            public string Reason
+            {
+                get { return reason; }
+                set { reason = value; }
+            }
         }
     }
 
