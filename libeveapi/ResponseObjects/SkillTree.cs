39f63aa8b6d16eb1219fcaf22bf03f95e3a9fed0
Lewis Vance <lewisvance@gmail.com>
2008-01-27 06:36:10 +0000

    added properties

--- libeveapi/ResponseObjects/SkillTree.cs
+++ libeveapi/ResponseObjects/SkillTree.cs
@@ -10,11 +10,22 @@ namespace libeveapi
     /// </summary>
     public class SkillTree : ApiResponse
     {
+        private Skill[] skills = new Skill[0];
+        private SkillGroup[] skillGroups = new SkillGroup[0];
+
         /// <summary>
         /// 
         /// </summary>
-        public Skill[] Skills = new Skill[0];
-        public SkillGroup[] SkillGroups = new SkillGroup[0];
+        public Skill[] Skills
+        {
+            get { return skills; }
+            set { skills = value; }
+        }
+        public SkillGroup[] SkillGroups
+        {
+            get { return skillGroups; }
+            set { skillGroups = value; }
+        }
 
         public static SkillTree FromXmlDocument(XmlDocument xmlDoc)
         {
@@ -79,50 +90,96 @@ namespace libeveapi
         /// </summary>
         public class Skill
         {
+            private string typeName;
+            private int groupId;
+            private int typeId;
+            private string description;
+            private int rank;
+            private RequiredSkill[] requiredSkills = new RequiredSkill[0];
+            private AttributeType primaryAttribute;
+            private AttributeType secondaryAttribute;
+            private SkillBonus[] skillBonuses = new SkillBonus[0];
+
             /// <summary>
             /// Name of the skill
             /// </summary>
-            public string TypeName;
+            public string TypeName
+            {
+                get { return typeName; }
+                set { typeName = value; }
+            }
 
             /// <summary>
             /// Unique identifier for the group this skill belongs to
             /// </summary>
-            public int GroupId;
+            public int GroupId
+            {
+                get { return groupId; }
+                set { groupId = value; }
+            }
 
             /// <summary>
             /// Unique identifier for this skill type
             /// </summary>
-            public int TypeId;
+            public int TypeId
+            {
+                get { return typeId; }
+                set { typeId = value; }
+            }
 
             /// <summary>
             /// information about the skill
             /// </summary>
-            public string Description;
+            public string Description
+            {
+                get { return description; }
+                set { description = value; }
+            }
 
             /// <summary>
             /// The training time multiplier of the skill
             /// </summary>
-            public int Rank;
+            public int Rank
+            {
+                get { return rank; }
+                set { rank = value; }
+            }
 
             /// <summary>
             /// The skills required to train this skill
             /// </summary>
-            public RequiredSkill[] RequiredSkills = new RequiredSkill[0];
+            public RequiredSkill[] RequiredSkills
+            {
+                get { return requiredSkills; }
+                set { requiredSkills = value; }
+            }
 
             /// <summary>
             /// The attrbute that has the most effect on the amount of time required to train this skill.
             /// </summary>
-            public AttributeType PrimaryAttribute;
+            public AttributeType PrimaryAttribute
+            {
+                get { return primaryAttribute; }
+                set { primaryAttribute = value; }
+            }
 
             /// <summary>
             /// The attribute that has the second most effect of the amount of time required to train this skill.
             /// </summary>
-            public AttributeType SecondaryAttribute;
+            public AttributeType SecondaryAttribute
+            {
+                get { return secondaryAttribute; }
+                set { secondaryAttribute = value; }
+            }
 
             /// <summary>
             /// The bonuses gained from training this skill.
             /// </summary>
-            public SkillBonus[] SkillBonuses = new SkillBonus[0];
+            public SkillBonus[] SkillBonuses
+            {
+                get { return skillBonuses; }
+                set { skillBonuses = value; }
+            }
         }
 
         /// <summary>
@@ -130,15 +187,26 @@ namespace libeveapi
         /// </summary>
         public class RequiredSkill
         {
+            private int typeId;
+            private int skillLevel;
+
             /// <summary>
             /// The unique identifier of this skill type.
             /// </summary>
-            public int TypeId;
+            public int TypeId
+            {
+                get { return typeId; }
+                set { typeId = value; }
+            }
 
             /// <summary>
             /// This skill level is required to train the parent skill.
             /// </summary>
-            public int SkillLevel;
+            public int SkillLevel
+            {
+                get { return skillLevel; }
+                set { skillLevel = value; }
+            }
         }
 
         /// <summary>
@@ -146,15 +214,26 @@ namespace libeveapi
         /// </summary>
         public class SkillGroup
         {
+            private string groupName;
+            private int groupId;
+
             /// <summary>
             /// The name of this skill group.
             /// </summary>
-            public string GroupName;
+            public string GroupName
+            {
+                get { return groupName; }
+                set { groupName = value; }
+            }
 
             /// <summary>
             /// The unique identifier for this skill group.
             /// </summary>
-            public int GroupId;
+            public int GroupId
+            {
+                get { return groupId; }
+                set { groupId = value; }
+            }
         }
 
         /// <summary>
@@ -162,15 +241,26 @@ namespace libeveapi
         /// </summary>
         public class SkillBonus
         {
+            private string bonusType;
+            private double bonusValue;
+
             /// <summary>
             /// Description of the bonus
             /// </summary>
-            public string BonusType;
+            public string BonusType
+            {
+                get { return bonusType; }
+                set { bonusType = value; }
+            }
 
             /// <summary>
             /// The amount of bonus gained from each level of the skill trained.
             /// </summary>
-            public double BonusValue;
+            public double BonusValue
+            {
+                get { return bonusValue; }
+                set { bonusValue = value; }
+            }
         }
 
         private static AttributeType mapStringToAttributeType(string s)
