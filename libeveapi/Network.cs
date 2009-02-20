4860343b5e8ef04ec5591ac3e968b0688946da1c
d.b.j.walschots <d.b.j.walschots@0af07104-5242-0410-b30e-b11728ae724a>
2009-02-20 23:42:25 +0000

    - Added a IApiDataRetriever interface for which implementations should be created.

--- libeveapi/Network.cs
+++ libeveapi/Network.cs
@@ -8,6 +8,24 @@ using System.Xml;
 
 namespace libeveapi
 {
+    ///<summary>
+    ///</summary>
+    public interface IApiDataRetriever
+    {
+        ///<summary>
+        /// Retrieves an XmlDocument from the given url.
+        /// pre-condition: the given url is not null.
+        /// post-condition: either an XmlDocument instance or null is returned.
+        ///</summary>
+        XmlDocument GetXml(Uri url);
+        ///<summary>
+        /// Retrieves an image from the given url.
+        /// pre-condition: the given url is not null.
+        /// post-condition: either an Image instance or null is returned.
+        ///</summary>
+        Image GetImage(Uri url);
+    }
+
     /// <summary>
     /// 
     /// </summary>
