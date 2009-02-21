9b973b96031bf619819e2694dd40360c7fff0597
d.b.j.walschots <d.b.j.walschots@0af07104-5242-0410-b30e-b11728ae724a>
2009-02-21 00:07:55 +0000

    - Added a new implementation for the network layer which should fully adhere to the interface.

--- libeveapi/Network.cs
+++ libeveapi/Network.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Net;
-using System.Text;
 using System.Xml;
 
 namespace libeveapi
@@ -15,15 +14,113 @@ namespace libeveapi
         ///<summary>
         /// Retrieves an XmlDocument from the given url.
         /// pre-condition: the given url is not null.
+        /// pre-condition: the given url's length is not 0.
         /// post-condition: either an XmlDocument instance or null is returned.
         ///</summary>
         XmlDocument GetXml(Uri url);
         ///<summary>
         /// Retrieves an image from the given url.
         /// pre-condition: the given url is not null.
+        /// pre-condition: the given url's length is not 0.
         /// post-condition: either an Image instance or null is returned.
         ///</summary>
         Image GetImage(Uri url);
+    }
+
+    ///<summary>
+    ///</summary>
+    internal class ApiDataRetriever : IApiDataRetriever
+    {
+        public XmlDocument GetXml( Uri url )
+        {
+            return GetObject( url, new StreamToXmlDocumentConverter() );
+        }
+
+        public Image GetImage( Uri url )
+        {
+            return GetObject( url, new StreamToImageConverter() );
+        }
+
+        private static T GetObject<T>( Uri url, IStreamConverter<T> converter ) where T : class
+        {
+            Debug.Assert( url != null );
+            Debug.Assert( url.ToString().Length > 0 );
+            Debug.Assert( converter != null );
+
+            return OpenUrl( url, converter );
+        }
+
+        /// <summary>
+        /// This function takes in a url and will return a stream of data from that url
+        /// Also takes into account the user-agent settings and any proxy settings
+        /// </summary>
+        /// <param name="url">The url of the image file to retrieve</param>
+        /// <param name="converter"></param>
+        /// <returns>A stream of data</returns>
+        private static T OpenUrl<T>(Uri url, IStreamConverter<T> converter ) where T : class
+        {
+            Debug.Assert( url != null );
+            Debug.Assert( url.ToString().Length > 0 );
+            Debug.Assert( converter != null );
+
+            WebClient wc = new WebClient();
+            wc.Headers.Add("user-agent", Network.eveNetworkClientSettings.userAgent);
+            if (Network.eveNetworkClientSettings.proxy != null)
+            {
+                // TODO Replace this...
+                wc.Proxy = Network.eveNetworkClientSettings.proxy;
+            }
+
+            try
+            {
+                using( Stream stream = wc.OpenRead( url ) )
+                {
+                    return converter.Convert( stream );
+                }
+            }
+            catch( WebException )
+            {
+                return null;
+            }
+        }
+    }
+
+    internal interface IStreamConverter<T>
+    {
+        T Convert( Stream stream );
+    }
+
+    internal class StreamToImageConverter : IStreamConverter<Image>
+    {
+        public Image Convert( Stream stream )
+        {
+            try
+            {
+                return Image.FromStream( stream, true, true );
+            }
+            catch( ArgumentException )
+            {
+                return null;
+            }
+        }
+    }
+
+    internal class StreamToXmlDocumentConverter : IStreamConverter<XmlDocument>
+    {
+        public XmlDocument Convert( Stream stream )
+        {
+            XmlDocument document = new XmlDocument();;
+
+            try
+            {
+                document.Load( stream );
+                return document;
+            }
+            catch( XmlException )
+            {
+                return null;
+            }
+        }
     }
 
     /// <summary>
