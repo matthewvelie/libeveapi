054c2843ea9471d430e4cc845ac24c32e628f2db
galford13x <galford13x@0af07104-5242-0410-b30e-b11728ae724a>
2010-03-11 01:07:07 +0000

    Added UnitTests/FacWarStatsTests.cs Added libeveapi/ResponseObjects/Parsers/CharFacWarStatsResponseParser.cs

---
+++ UnitTests/FacWarStatsTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using libeveapi;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class FacWarStatsTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Utility.UseLocalUrls();
+        }
+
+        [Test]
+        public void GetCharFacWarStatsTests()
+        {
+            FacWarStats stats = new FacWarStats();
+            stats.CharacterFactionWarStats = EveApi.GetCharacterFactionWarStats(123, 456, "apiKey");
+
+            Assert.AreEqual(500001, stats.CharacterFactionWarStats.FactionID);
+            Assert.AreEqual("Caldari State", stats.CharacterFactionWarStats.FactionName);
+            Assert.AreEqual(new DateTime(2008, 06, 10, 22, 10, 00), stats.CharacterFactionWarStats.Enlisted);
+            Assert.AreEqual(4, stats.CharacterFactionWarStats.CurrentRank);
+            
+            Assert.AreEqual(4, stats.CharacterFactionWarStats.HighestRank);
+            Assert.AreEqual(0, stats.CharacterFactionWarStats.KillsYesterday);
+            Assert.AreEqual(0, stats.CharacterFactionWarStats.KillsLastWeek);
+            Assert.AreEqual(0, stats.CharacterFactionWarStats.KillsTotal);
+            Assert.AreEqual(0, stats.CharacterFactionWarStats.VictoryPointsYesterday);
+            Assert.AreEqual(1044, stats.CharacterFactionWarStats.VictoryPointsLastWeek);
+            Assert.AreEqual(0, stats.CharacterFactionWarStats.VictoryPointsTotal);
+
+        }
+
+        [Test]
+        public void FacWarStatsTestsPersist()
+        {
+            ResponseCache.Clear();
+
+            FacWarStats stats = new FacWarStats();
+            stats.CharacterFactionWarStats = EveApi.GetCharacterFactionWarStats(123, 456, "apiKey");
+
+            ResponseCache.Save("ResponseCache.xml");
+            ResponseCache.Clear();
+            ResponseCache.Load("ResponseCache.xml");
+            FacWarStats statsCached = new FacWarStats();
+            statsCached.CharacterFactionWarStats = EveApi.GetCharacterFactionWarStats(987, 654, "apiKey");
+
+            Assert.AreEqual(stats.CharacterFactionWarStats.CachedUntilLocal, statsCached.CachedUntilLocal);
+
+            Assert.AreEqual(stats.CharacterFactionWarStats.FactionID, statsCached.CharacterFactionWarStats.FactionID);
+            Assert.AreEqual(stats.CharacterFactionWarStats.FactionName, statsCached.CharacterFactionWarStats.FactionName);
+            Assert.AreEqual(stats.CharacterFactionWarStats.Enlisted, statsCached.CharacterFactionWarStats.Enlisted);
+            Assert.AreEqual(stats.CharacterFactionWarStats.CurrentRank, statsCached.CharacterFactionWarStats.CurrentRank);
+            Assert.AreEqual(stats.CharacterFactionWarStats.KillsYesterday, statsCached.CharacterFactionWarStats.KillsYesterday);
+            Assert.AreEqual(stats.CharacterFactionWarStats.KillsLastWeek, statsCached.CharacterFactionWarStats.KillsLastWeek);
+            Assert.AreEqual(stats.CharacterFactionWarStats.KillsTotal, statsCached.CharacterFactionWarStats.KillsTotal);
+            Assert.AreEqual(stats.CharacterFactionWarStats.VictoryPointsYesterday, statsCached.CharacterFactionWarStats.VictoryPointsYesterday);
+            Assert.AreEqual(stats.CharacterFactionWarStats.VictoryPointsLastWeek, statsCached.CharacterFactionWarStats.VictoryPointsLastWeek);
+            Assert.AreEqual(stats.CharacterFactionWarStats.VictoryPointsTotal, statsCached.CharacterFactionWarStats.VictoryPointsTotal);
+        }
+
+    }
+}
---
+++ libeveapi/ResponseObjects/Parsers/CharFacWarStatsResponseParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace libeveapi.ResponseObjects.Parsers
+{
+    internal class CharFacWarStatsResponseParser : IApiResponseParser<FacWarStats.CharFacWarStatsItem>
+    {
+        public FacWarStats.CharFacWarStatsItem Parse(XmlDocument xmlDocument)
+        {
+            this.CheckVersion(xmlDocument);
+            FacWarStats stats = new FacWarStats();
+            stats.ParseCommonElements(xmlDocument);
+
+            stats.CharacterFactionWarStats.FactionID = Convert.ToInt32(xmlDocument.SelectSingleNode("//factionID").InnerText);
+            stats.CharacterFactionWarStats.FactionName = xmlDocument.SelectSingleNode("//factionName").InnerText;
+            stats.CharacterFactionWarStats.Enlisted = Convert.ToDateTime(xmlDocument.SelectSingleNode("//enlisted").InnerText);
+            stats.CharacterFactionWarStats.CurrentRank = Convert.ToInt32(xmlDocument.SelectSingleNode("//currentRank").InnerText);
+            stats.CharacterFactionWarStats.HighestRank = Convert.ToInt32(xmlDocument.SelectSingleNode("//highestRank").InnerText);
+            stats.CharacterFactionWarStats.KillsYesterday = Convert.ToInt32(xmlDocument.SelectSingleNode("//killsYesterday").InnerText);
+            stats.CharacterFactionWarStats.KillsLastWeek = Convert.ToInt32(xmlDocument.SelectSingleNode("//killsLastWeek").InnerText);
+            stats.CharacterFactionWarStats.KillsTotal = Convert.ToInt32(xmlDocument.SelectSingleNode("//killsTotal").InnerText);
+            stats.CharacterFactionWarStats.VictoryPointsYesterday = Convert.ToInt32(xmlDocument.SelectSingleNode("//victoryPointsYesterday").InnerText);
+            stats.CharacterFactionWarStats.VictoryPointsLastWeek = Convert.ToInt32(xmlDocument.SelectSingleNode("//victoryPointsLastWeek").InnerText);
+            stats.CharacterFactionWarStats.VictoryPointsTotal = Convert.ToInt32(xmlDocument.SelectSingleNode("//victoryPointsTotal").InnerText);
+
+            return stats.CharacterFactionWarStats;
+        }
+
+        public void CheckVersion(XmlDocument xmlDocument)
+        {
+            if (FacWarStats.VersionCheck)
+            {
+                string version = xmlDocument.SelectSingleNode("//eveapi").Attributes["version"].InnerText;
+                if (version.CompareTo(FacWarStats.API_VERSION) != 0)
+                {
+                    throw new ApiVersionException(version, FacWarStats.API_VERSION);
+                }
+            }
+        }
+    }
+}
